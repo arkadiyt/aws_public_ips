595c234b761dcd78a110745ed631f7f17bce9530
Arkadiy Tetelman <arkadiyt@gmail.com>
2018-05-20 15:37:09 -0700

    Add more Redshift tests

--- lib/aws_public_ips/checks/redshift.rb
+++ lib/aws_public_ips/checks/redshift.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'aws-sdk-redshift'
+require 'aws_public_ips/utils'
 
 module AwsPublicIps
   module Checks
@@ -8,17 +9,24 @@ module AwsPublicIps
       def self.run
         client = ::Aws::Redshift::Client.new
 
-        # TODO(arkadiy) update copy from RDS
-        # Redshift clusters can only be launched into VPCs. They can be marked as `publicly_accessible`,
-        # in which case the VPC must have an Internet Gateway attached, and the DNS endpoint will
-        # resolve to a public ip address
+        # Redshift clusters can be launched into VPCs or into Classic mode.
+        # In classic mode they are always public.
+        # In VPC mode they can be marked as `publicly_accessible` or not - if they are then its VPC must have
+        # an Internet Gateway attached, and the DNS endpoint will resolve to a public ip address.
         client.describe_clusters.flat_map do |response|
           response.clusters.flat_map do |cluster|
             next [] unless cluster.publicly_accessible
+
+            if cluster.endpoint.nil?
+              raise StandardError, "Redshift cluster '#{cluster.cluster_identifier}' has a nil endpoint. This likely" \
+                ' means the cluster is being brought up right now.'
+            end
+
             {
               id: cluster.cluster_identifier,
               hostname: cluster.endpoint.address,
-              ip_addresses: cluster.cluster_nodes.map(&:public_ip_address)
+              ip_addresses: cluster.cluster_nodes.map(&:public_ip_address) +
+                ::AwsPublicIps::Utils.resolve_hostname(cluster.endpoint.address)
             }
           end
         end
--- spec/aws_public_ips/checks/redshift_spec.rb
+++ spec/aws_public_ips/checks/redshift_spec.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 describe ::AwsPublicIps::Checks::Redshift do
-  it 'should return redshift ips' do
+  it 'should return redshift ips for classic public clusters' do
     stub_request(:post, 'https://redshift.us-east-1.amazonaws.com')
-      .to_return(body: ::IO.read('spec/fixtures/redshift.xml'))
+      .to_return(body: ::IO.read('spec/fixtures/redshift-classic-public.xml'))
 
-    # TODO(arkadiy) want to launch VPC cluster but can't - try again later
     expect(subject.run).to eq([
       {
         id: 'classic',
@@ -13,5 +12,34 @@ describe ::AwsPublicIps::Checks::Redshift do
         ip_addresses: %w[54.167.97.240 54.91.252.196 54.242.227.110]
       }
     ])
+  end
+
+  it 'should return redshift ips for vpc public clusters' do
+    stub_request(:post, 'https://redshift.us-east-1.amazonaws.com')
+      .to_return(body: ::IO.read('spec/fixtures/redshift-vpc-public.xml'))
+
+    stub_dns(
+      'vpc-public-2.csorkyt5dk7h.us-east-1.redshift.amazonaws.com' => %w[54.156.7.130]
+    )
+    expect(subject.run).to eq([
+      {
+        id: 'vpc-public-2',
+        hostname: 'vpc-public-2.csorkyt5dk7h.us-east-1.redshift.amazonaws.com',
+        ip_addresses: %w[52.44.170.10 54.209.53.206 54.208.75.129 54.156.7.130]
+      }
+    ])
+  end
+
+  it 'should return nothing for vpc private clusters' do
+    stub_request(:post, 'https://redshift.us-east-1.amazonaws.com')
+      .to_return(body: ::IO.read('spec/fixtures/redshift-vpc-private.xml'))
+
+    expect(subject.run).to eq([])
+  end
+
+  it 'should handle clusters with a nil endpoint' do
+    stub_request(:post, 'https://redshift.us-east-1.amazonaws.com')
+      .to_return(body: ::IO.read('spec/fixtures/redshift-empty-endpoint.xml'))
+    expect { subject.run }.to raise_error(StandardError, /has a nil endpoint/)
   end
 end
